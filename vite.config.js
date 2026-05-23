552a424cd40f2eda4a79be1135fbcc0748217b56
Pooja Chandrikapure <30poojachandrikapure@gmail.com>
2026-05-23 11:16:34 +0530

    fix: build errors

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools(),],
+  plugins: [vue(), tailwindcss()],
 
   resolve: {
     alias: {
